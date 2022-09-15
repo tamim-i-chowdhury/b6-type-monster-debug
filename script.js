8f21ce830eb85ab7ca5fddd6584617509fd5149e
tamim-i-chowdhury <tamim.c183@gmail.com>
2022-09-15 14:59:51 +0600

    finishing alert now showing at the middle of the view-port

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ const gameOver = () => {
     <h1>Finished!</h1>
     <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
     <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
-    <button onclick="closeModal()">Close</button>
+    <button style="margin-top: 10px;" onclick="closeModal()">Close</button>
   `;
 
   addHistory(questionText, timeTaken, errorCount);
