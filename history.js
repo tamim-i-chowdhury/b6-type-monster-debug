c1196a840e43c146a27254c282eb7d85ff6e073c
tamim-i-chowdhury <tamim.c183@gmail.com>
2022-09-15 16:12:56 +0600

    all the bugs insdie the js file fixed now

--- history.js
+++ history.js
@@ -17,7 +17,6 @@ function addHistory(questionText, timeTaken, errorCount) {
   let previousTests = JSON.parse(localStorage.getItem("testHistory")) || [];
   previousTests.push({ questionText, timeTaken, errorCount });
   localStorage.setItem("testHistory", JSON.stringify(previousTests));
-
   displayHistory();
 }
 
@@ -31,7 +30,9 @@ function displayHistory() {
 
     newRow.innerHTML = `
   <h3>${test.questionText}</h3>
-  <p>You took: <span class="bold">${test.timeTaken}</span> seconds</p>
+  <p>You took: <span class="bold red">${Math.round(
+    test.timeTaken
+  )}</span> seconds</p>
     <p>You made <span class="bold red">${test.errorCount}</span> mistakes</p>
   `;
 
